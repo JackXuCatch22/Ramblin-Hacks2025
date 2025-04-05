ce8b6ffe94c16355c0480dc44f66de6335a9d526
eldridgeS <eldridgesurianto1@gmail.com>
2025-04-05 16:45:08 -0400

    html

--- frontend/FinPal/settings.py
+++ frontend/FinPal/settings.py
@@ -114,7 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# If you're not using collectstatic for production, add:
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
